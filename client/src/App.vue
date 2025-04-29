6e065f758589fc940d932df41cb0c0534be5f376
VlaDick2008 <sochukushat@yandex.ru>
2025-04-29 14:14:29 +0300

    init

---
+++ client/src/App.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import HelloWorld from './components/HelloWorld.vue'
+import TheWelcome from './components/TheWelcome.vue'
+</script>
+
+<template>
+  <main></main>
+</template>
+
+<style>
+@import "tailwindcss";
+</style>

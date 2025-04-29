0609398f1a27391c4104e283a1c627dd6630f6ae
VlaDick2008 <sochukushat@yandex.ru>
2025-04-29 21:45:41 +0300

    fix url and imageUrl bug, changed seed script functionality

--- api/NewsApi.Utils/Seed.cs
+++ api/NewsApi.Utils/Seed.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Text.Json;
+using LoremNET;
+using Microsoft.AspNetCore.Mvc.Routing;
 using NewsApi.Models;
 
 namespace NewsApi.Utils
@@ -14,15 +16,32 @@ namespace NewsApi.Utils
             var items = new List<NewsItem>();
             var rnd = new Random();
 
+            var imageRand = new[]
+            {
+                "",
+                "neo/",
+                "millie/",
+                "millie_neo/",
+                "neo_banana/",
+                "neo_2/",
+                "bella/",
+            };
+
+            int nameCount = rnd.Next(5, 20);
+            int descriptionCount = rnd.Next(25, 155);
+
             for (int i = 1; i <= count; i++)
             {
+                int imageIndex = rnd.Next(imageRand.Length);
+
                 items.Add(
                     new NewsItem
                     {
                         Id = i,
-                        Name = $"Mock News Title {i}",
-                        Description = $"Description for mock news {i}.",
-                        Url = $"https://picsum.photos/seed/news{i}/600/400",
+                        Name = Lorem.Words(nameCount),
+                        Description = Lorem.Words(descriptionCount),
+                        Url = $"https://placecats.com/{imageRand[imageIndex]}600/400",
+                        ImageUrl = $"https://placecats.com/{imageRand[imageIndex]}600/400",
                         PublishedAt = DateTime.UtcNow.AddDays(-rnd.Next(0, 30)),
                     }
                 );
